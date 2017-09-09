4ab29e0924aa26f14687f7a1451e22f1bdfb9745
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-09-09 18:56:16 +0200

    commit

--- Other Stuff/Programming y2p1/Assets/Move.cs
+++ Other Stuff/Programming y2p1/Assets/Move.cs
@@ -13,11 +13,16 @@ public class Move : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        if (Input.GetButton("Fire1"))
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X"), 0, Space.World);
+        }
+
+
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * 0.3f, Input.GetAxisRaw("Mouse ScrollWheel") * 5, vertical * 0.3f);
 
-        transform.Translate(horizontal * 0.1f, 0, vertical * 0.1f);
-
-        transform.Rotate(0, Input.GetAxis("Mouse X"), 0, Space.World);
     }
 }
