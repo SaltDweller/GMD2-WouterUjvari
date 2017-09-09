9462a929f216dcebc54fae8d98f3808852c4191c
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-09-09 15:35:01 +0200

    move

---
+++ Other Stuff/Programming y2p1/Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontal * 0.1f, 0, vertical * 0.1f);
+
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0, Space.World);
+    }
+}
