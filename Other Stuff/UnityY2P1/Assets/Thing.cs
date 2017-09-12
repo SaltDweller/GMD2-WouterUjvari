472c74d312bca5f991f52b2526c6572a95ab538e
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-09-12 08:32:08 +0200

    post

---
+++ Other Stuff/UnityY2P1/Assets/Thing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thing : MonoBehaviour {
+
+
+    public List<int> GetOddNumbers(List<int> myOtherList)
+    {
+        List<int> getOddNumbers = new List<int>();
+
+        for (int i = 0; i < myOtherList.Count; i++)
+        {
+            if (myOtherList[i] % 2 == 1)
+            {
+                getOddNumbers.Add(myOtherList[i]);            
+            }
+
+        }
+        return getOddNumbers;
+    }
+}
+
+
